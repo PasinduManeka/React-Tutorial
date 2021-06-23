08b8566bb6907ec343adf73674e9b633250eb659
PasinduManeka <manekaherat815@gmail.com>
2021-06-24 00:22:08 +0530

    hook status

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,14 @@
+import {useState} from 'react';
 const Home = () => {
+
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(20);
+
+    const handleClickChange = () =>{
+        setName('Pasindu');
+        setAge(23);
+    }
+
     //button click event handler
     const  handleClick = () =>{
         console.log('Helo, Fellows.');
@@ -12,6 +22,9 @@ const Home = () => {
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <p>He is {name}. He is {age} years old.</p>
+            {/*Chaneg Button*/}
+            <button class="btn btn-outline-success" on onClick={handleClickChange}>Click me to change the name!</button><br /><br />
             {/*Bitton1*/}
             <button class="btn btn-outline-secondary" onClick={handleClick}>Click me !</button><br /><br />
             {/*Button 2*/}
