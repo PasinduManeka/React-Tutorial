50f216e53318b1ea8dee1da519384ce71278c07c
PasinduManeka <manekaherat815@gmail.com>
2021-06-23 22:35:42 +0530

    Create a button to pass parameters.

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,17 @@ const Home = () => {
     } 
 
     //button2 click event handler
-    /*const handleClickAgain = (name) =>{
+    const handleClickAgain = (name) =>{
         console.log('hello '+name);
-    }*/
+    }
 
     return (
         <div className="home">
             <h2>Home Page</h2>
             {/*Bitton1*/}
-            <button class="btn btn-outline-secondary" onClick={handleClick}>Click me</button><br /><br />
+            <button class="btn btn-outline-secondary" onClick={handleClick}>Click me !</button><br /><br />
+            {/*Button 2*/}
+            <button class="btn btn-outline-secondary" onClick={()=>handleClickAgain('Pasindu')}>Click me again !</button>
         </div>
     );
 }
