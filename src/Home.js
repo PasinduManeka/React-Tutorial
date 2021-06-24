93110980e5ba2c273acc972d21c8dfaf5fcf4003
PasinduManeka <manekaherat815@gmail.com>
2021-06-24 23:11:52 +0530

    Display created blogs using map.

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,21 @@
 import {useState} from 'react';
 const Home = () => {
 
-    const [name, setName] = useState('Mario');
-    const [age, setAge] = useState(20);
-
-    const handleClickChange = () =>{
-        setName('Pasindu');
-        setAge(23);
-    }
-
-    //button click event handler
-    const  handleClick = () =>{
-        console.log('Helo, Fellows.');
-    } 
-
-    //button2 click event handler
-    const handleClickAgain = (name) =>{
-        console.log('hello '+name);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return (
         <div className="home">
-            <h2>Home Page</h2>
-            <p>He is {name}. He is {age} years old.</p>
-            {/*Chaneg Button*/}
-            <button class="btn btn-outline-success" on onClick={handleClickChange}>Click me to change the name!</button><br /><br />
-            {/*Bitton1*/}
-            <button class="btn btn-outline-secondary" onClick={handleClick}>Click me !</button><br /><br />
-            {/*Button 2*/}
-            <button class="btn btn-outline-secondary" onClick={()=>handleClickAgain('Pasindu')}>Click me again !</button>
-        </div>
+            {blogs.map((blog) => (
+              <div className="blog-preview card shadow p-3 mb-5 bg-white rounded" key={blog.id}>
+                  <h2 class="text-danger">{blog.title}</h2>
+                  <p>Written by:{blog.author}</p>
+              </div>
+            ))}
+        <br /></div>
     );
 }
  
