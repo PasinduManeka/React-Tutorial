5e8e6dabf0f3e15e215f357e9e68122366da34df
PasinduManeka <manekaherat815@gmail.com>
2021-07-08 01:39:54 +0530

    useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,8 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+    
+    const [name, setName] = useState('mario');
 
     //First hook
     const handleDelete = (id) => {
@@ -15,14 +17,17 @@ const Home = () => {
 
     }
 
-    //second hook
+    //second hook. We can do any authentication, fetch data 
     useEffect(()=>{
         console.log('Use effect ran.');
-    });
+        console.log(name);
+    }, [name]);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs.." handleDelete={handleDelete} />
+            <button class="btn btn-outline-primary" onClick={()=>setName('lungi')}>Change the name</button>
+            <p>{name}</p>
         </div>
     );
 }
