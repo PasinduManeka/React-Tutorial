0bcff5dca18aaefc9401b3dab9edeffeabf15da5
PasinduManeka <manekaherat815@gmail.com>
2021-07-09 02:50:58 +0530

    Router links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The dojo blogs</h1>
             <div className="links">
-                <a class="mask rgba-red-strong" href="/">Home</a>
-                <a class="bg-danger text-white rounded" href="/NewBLog">New Blog</a>
+                <Link class="mask rgba-red-strong" to="/">Home</Link>
+                <Link class="bg-danger text-white rounded" to="/NewBLog">New Blog</Link>
             </div>
         </nav>
     );
