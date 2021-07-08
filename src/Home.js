47cb1edb466a08bd8208c8364b9389de7fd37f67
PasinduManeka <manekaherat815@gmail.com>
2021-07-08 19:29:36 +0530

    loading message

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,9 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
     
     //const [name, setName] = useState('mario');
-
-    
-    
-
     //second hook. We can do any authentication, fetch data 
     useEffect(()=>{
         fetch('http://localhost:8000/blogs')
@@ -17,11 +14,13 @@ const Home = () => {
             })
             .then(data =>{
                 setBlogs(data);
+                setIsPending(false);
             });
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div class="alert alert-primary d-inline-flex p-2"><strong>Loading...</strong></div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs.." />}
             
         </div>
